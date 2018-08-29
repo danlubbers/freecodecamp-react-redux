d3075b3991934f7e2ece6dd6f87540b865c97ece
Dan Lubbers <lubbers.dan@gmail.com>
2018-08-29 13:52:29 -0400

    Finished lesson

--- react-redux-practice.jsx
+++ react-redux-practice.jsx
@@ -162,4 +162,150 @@
         // change code below this line
         let ConnectedComponent = connect(mapStateToProps, mapDispatchToProps)(Presentational)
 
-        
+
+// 8. Connect Redux to the Messages App
+
+        const Provider = ReactRedux.Provider;
+        const connect = ReactRedux.connect;
+
+        // define the Container component here:
+        const Container = connect(mapStateToProps, mapDispatchToProps)(Presentational);
+
+        class AppWrapper extends React.Component {
+        constructor(props) {
+            super(props);
+        }
+        render() {
+            // complete the return statement:
+            return(
+            <Provider store={store}>
+            <Container />
+            </Provider>
+            )
+        }
+        };
+
+
+// 9. Extract Local State into Redux
+
+            // Redux:
+        const ADD = 'ADD';
+
+        const addMessage = (message) => {
+        return {
+            type: ADD,
+            message: message
+        }
+        };
+
+        const messageReducer = (state = [], action) => {
+        switch (action.type) {
+            case ADD:
+            return [
+                ...state,
+                action.message
+            ];
+            default:
+            return state;
+        }
+        };
+
+        const store = Redux.createStore(messageReducer);
+
+        // React:
+        const Provider = ReactRedux.Provider;
+        const connect = ReactRedux.connect;
+
+        // Change code below this line
+        class Presentational extends React.Component {
+        constructor(props) {
+            super(props);
+            this.state = {
+            input: '',
+            }
+            this.handleChange = this.handleChange.bind(this);
+            this.submitMessage = this.submitMessage.bind(this);
+        }
+        handleChange(event) {
+            this.setState({
+            input: event.target.value
+            });
+        }
+        submitMessage() {
+            this.props.submitNewMessage(this.state.input);
+            this.setState({
+            input: '',
+            });
+        }
+        render() {
+            return (
+            <div>
+                <h2>Type in a new Message:</h2>
+                <input
+                value={this.state.input}
+                onChange={this.handleChange}/><br/>
+                <button onClick={this.submitMessage}>Submit</button>
+                <ul>
+                {this.props.messages.map( (message, idx) => {
+                    return (
+                        <li key={idx}>{message}</li>
+                    )
+                    })
+                }
+                </ul>
+            </div>
+            );
+        }
+        };
+        // Change code above this line
+
+        const mapStateToProps = (state) => {
+        return {messages: state}
+        };
+
+        const mapDispatchToProps = (dispatch) => {
+        return {
+            submitNewMessage: (message) => {
+            dispatch(addMessage(message))
+            }
+        }
+        };
+
+        const Container = connect(mapStateToProps, mapDispatchToProps)(Presentational);
+
+        class AppWrapper extends React.Component {
+        render() {
+            return (
+            <Provider store={store}>
+                <Container/>
+            </Provider>
+            );
+        }
+        };
+
+// 10. Moving Forward From Here
+
+        // import React from 'react'
+        // import ReactDOM from 'react-dom'
+        // import { Provider, connect } from 'react-redux'
+        // import { createStore, combineReducers, applyMiddleware } from 'redux'
+        // import thunk from 'redux-thunk'
+
+        // import rootReducer from './redux/reducers'
+        // import App from './components/App'
+
+        // const store = createStore(
+        //   rootReducer,
+        //   applyMiddleware(thunk)
+        // );
+
+        // ReactDOM.render(
+        //   <Provider store={store}>
+        //     <App/>
+        //   </Provider>,
+        //   document.getElementById('root')
+        // );
+
+        // change code below this line
+        console.log('Now I know React and Redux!')
+                        
